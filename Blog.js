0858efb902e93396f46b0405526e8035eaadf928
BobRoyler <bobroyler@yandex.com>
2023-01-08 00:56:50 +0000

    Initial Version

---
+++ Blog.js
@@ -0,0 +1,7 @@
+const body = document.querySelector("body"),
+      nav = document.querySelector("nav"),
+      navBar = document.querySelector(".nav-bar");
+
+    navBar.addEventListener("click", () =>{
+             nav.classList.add("show");
+})
---
+++ Muscle.js
@@ -0,0 +1,77 @@
+//Slider automatic//
+//Slider automatic//
+var slideIndex = 0;
+var slider = document.querySelector(".slider");
+var slides = document.querySelector(".slides");
+var slide = document.querySelectorAll(".slide");
+var dots = document.querySelectorAll(".dots label")
+var time = 5000;
+
+function plusslide(position) {
+    slideIndex += position;
+
+    if (slideIndex > slide.length) {
+        slideIndex = 1;
+    }
+    else if (slideIndex < 1) {
+        slideIndex = slide.length;
+    }
+
+    // Defaultly active class is removed from all dots
+    for (let i = 0; i < dots.length; i++) {
+        const element = dots[i];
+        element.classList.remove("dot-active");
+    }
+
+    slides.style.left = `-${slideIndex - 1}00%`;
+    dots[slideIndex - 1].classList.add("dot-active");
+}
+
+function currentslide(index) {
+    if (index > slide.length) {
+        index = 1;
+    }
+    else if (index < 1) {
+        index = slide.length;
+    }
+
+    // Defaultly active class is removed from all dots
+    for (let i = 0; i < dots.length; i++) {
+        const element = dots[i];
+        element.classList.remove("dot-active");
+    }
+
+    slides.style.left = `-${index - 1}00%`;
+    dots[index - 1].classList.add("dot-active");
+
+    slideIndex = index;
+}
+
+
+setInterval(  function showslide() {
+    slideIndex++;
+
+    if (slideIndex > slide.length) {
+        slideIndex = 1;
+    }
+    else if (slideIndex < 1) {
+        slideIndex = slide.length;
+    }
+
+    // Defaultly active class is removed from all dots
+    for (let i = 0; i < dots.length; i++) {
+        var element = dots[i];
+        element.classList.remove("dot-active");
+    }
+
+    slides.style.left = `-${slideIndex - 1}00%`;
+    dots[slideIndex - 1].classList.add("dot-active");
+
+} , time);   // Change every image after 3 seconds
+
+
+
+
+
+
+
