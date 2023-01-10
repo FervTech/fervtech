0b81ee4a8aab8d612140ae882b40faacbf35b9ea
BobRoyler <bobroyler@yandex.com>
2023-01-10 01:19:01 +0000

    Initial Version

--- Blog.js
+++ Blog.js
@@ -1,7 +1,30 @@
-const body = document.querySelector("body"),
-      nav = document.querySelector("nav"),
-      navBar = document.querySelector(".nav-bar");
 
-    navBar.addEventListener("click", () =>{
-             nav.classList.add("show");
-})
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+});
--- Muscle.js
+++ Muscle.js
@@ -70,8 +70,38 @@ setInterval(  function showslide() {
 } , time);   // Change every image after 3 seconds
 
 
+// Swiper
 
 
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+});
 
 
 
