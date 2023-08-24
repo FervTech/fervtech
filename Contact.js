a41a79870f7b11771e76236eae8c9a7f15f1a582
BobRoyler <bobroyler@yandex.com>
2023-08-24 17:51:57 +0000

    Merge remote-tracking branch 'origin/master'

--- Contact.js
+++ Contact.js
@@ -16,6 +16,7 @@ angleDown.onclick = function (){
 window.addEventListener("scroll", function (){
     var nav = document.querySelector(".nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
+
 })
 
 //------------Scroll-Up------------//
