810b16d5933f1da3f2555017b9fd0f1268505b72
BobRoyler <bobroyler@yandex.com>
2024-04-01 14:47:44 +0000

    Merge remote-tracking branch 'origin/master'

--- js/muscle.js
+++ js/muscle.js
@@ -1,14 +1,4 @@
-// Remove .html extension from URLs
-function removeHtmlExtension() {
-    var currentUrl = window.location.href;
-    if (currentUrl.endsWith('.html')) {
-        var newUrl = currentUrl.replace('.html', '');
-        window.history.pushState({}, '', newUrl);
-    }
-}
 
-// Call the function on page load
-removeHtmlExtension();
 
 
 
